8a665363b62184cba035e1a0e8638e1713a2a52e
4-Bit-Player <mrx0133@web.de>
2025-05-06 12:41:26 +0200

    Improved the temp input class a bit and added type hinting

--- player/keyinput_index_class.py
+++ player/keyinput_index_class.py
@@ -4,25 +4,91 @@ from decoration.deco import line_r
 
 
 class TempInput:
-    def __init__(self, val:str=""):
-        if val == "":
+    """
+    Class for saving the state of the user input
+    """
+    def rm_last_char(self) -> None:
+        """
+        Removes the last character from its list.
+        :return: Returns nothing
+        """
+        pass
+    def text(self) -> str:
+        """
+        Returns the content of itself as a string.
+        :return: A string
+        """
+        pass
+    def clear(self) -> None:
+        """
+        Clears its list.
+        :return: Nothing
+        """
+        pass
+    @property
+    def size(self) -> int:
+        """
+        :return: The size of its list
+        """
+        pass
+    @property
+    def input_list(self) -> list[str]:
+        """
+        :return: A copy of the internal list of chars.
+        """
+        pass
+
+
+class TempInput(TempInput):
+    def __init__(self, val:list=None):
+        if val is None:
             self._temp_input:list[str] = []
             self._size:int = 0
         else:
-            self._temp_input:list[str] = [x for x in val]
+            self._temp_input:list[str] = val
             self._size:int = len(self._temp_input)
 
-    def __iadd__(self, other:str):
-        self._temp_input.append(other)
-        self._size += 1
+    def __iadd__(self, other:str|list[str]|TempInput):
+        if isinstance(other, str):
+            self._temp_input.append(other)
+            self._size += 1
+
+        elif isinstance(other, list):
+            for thing in other:
+                if isinstance(thing, str):
+                    continue
+                raise ValueError(f"Trying to add the wrong type to a TempInput instance using a list.\nThe list has to contain only strings, not {type(thing)}.")
+            self._temp_input += other[:]
+            self._size += len(other)
+
+        elif isinstance(other, TempInput):
+            self._temp_input += other.input_list
+            self._size += other.size
         return self
 
-    def __add__(self, other):
-        return self._temp_input[:] + [other]
+    def __add__(self, other:str|list[str]|TempInput) -> TempInput:
+        if isinstance(other, str):
+            return TempInput(self._temp_input[:] + [other])
+
+        if isinstance(other, list):
+            return TempInput(self._temp_input[:] + other)
+
+        if not isinstance(other, TempInput):
+            raise NotImplementedError(f"No implementation for adding with Type '{type(other)}'")
+
+        return TempInput(self._temp_input[:]+other.input_list)
 
 
     def __len__(self):
         return len(self._temp_input)
+
+
+    def __repr__(self):
+        return f"TempInput; size: {self._size}; chars: {self._temp_input}"
+
+    def __str__(self):
+        return "".join(self._temp_input)
+
 
     def rm_last_char(self):
         if len(self._temp_input) == 0:
@@ -41,7 +107,9 @@ class TempInput:
     def size(self):
         return self._size
 
-
+    @property
+    def input_list(self)->list[str]:
+        return self._temp_input[:]
 
 
 
